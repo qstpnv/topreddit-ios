9b121b8566645efce343979e5931c181bcf2d039
qarthur <arthur.stpnv@gmail.com>
2019-04-03 22:04:32 +0300

    project init

---
+++ topreddit/Controllers/Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  topreddit
+//
+//  Created by Arthur Stepanov on 03.04.2019.
+//  Copyright © 2019 Arthur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
